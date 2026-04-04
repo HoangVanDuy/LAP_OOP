4bec98c5f5a590d680dc6f914b1ba801b76961e1
HoangDuy2222 <hoangduydc2222@gmail.com>
2026-04-05 00:28:49 +0700

    lap 2

--- LAP02/AimsProject/src/Aims.java
+++ LAP02/AimsProject/src/Aims.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class Aims {
     public static void main(String[] args) {
-        //Create a new cart
+        // Create a new cart
         Cart anOrder = new Cart();
 
-        //Create new DVD objects and add them to the cart
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King","Animation",
+        // Create new DVD objects and add them to the cart
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation",
                 "Roger Allers", 87, 19.95f);
         anOrder.addDigitalVideoDisc(dvd1);
 
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars","Science Fiction",
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction",
                 "George Lucas", 87, 24.95f);
         anOrder.addDigitalVideoDisc(dvd2);
 
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin","Animation",18.99f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
         anOrder.addDigitalVideoDisc(dvd3);
 
         // View
@@ -26,5 +26,6 @@ public class Aims {
         String title = scanner.nextLine();
         anOrder.removeDigitalVideoDisc(title);
         anOrder.displayCart();
+        scanner.close();
     }
 }
